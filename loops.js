40b75c777a2433237ec72754d9af35851691bffe
Christian Weiss <worldmover777@gmail.com>
2018-01-28 18:06:19 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,12 @@
-var array
+function forLoop(array){
 
 for(var i=0;i<25;i++){
   if(i===1){
-    array="I am 1 strange loop."
+    array=array+"I am 1 strange loop."
   }
   else {
     array=array+"I am ${i} strange loops"
   }
+}
 }
 return array
