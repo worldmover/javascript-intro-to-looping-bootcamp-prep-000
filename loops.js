6f2fbfff088bb53fbd356b51496c45304a1dd27a
Christian Weiss <worldmover777@gmail.com>
2018-01-28 18:31:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,5 +18,11 @@ function whileLoop(number){
 }
 
 function doWhileLoop(array){
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
